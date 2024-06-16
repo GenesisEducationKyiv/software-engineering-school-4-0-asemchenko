2969b9eae5594542b0c31b8a6117bf085b83997e
Andrii Semchenko <a7chenko@gmail.com>
2024-06-16 20:56:19 +0300

    Add second test for currency_service

--- internal/service/currency_service_test.go
+++ internal/service/currency_service_test.go
@@ -24,11 +24,46 @@ func TestCurrencyService_GetUSDtoUAHRate_whenNoCachedValue_shouldLoadAndSaveRate
 	// perform test
 	rate, err := currencyService.GetUSDtoUAHRate()
 
+	assertRate(t, err, 8.0, rate)
+}
+
+func assertRate(t *testing.T, err error, expected float64, actual float64) {
 	// assert result
+	assertNoError(t, err)
+	if actual != expected {
+		t.Fatalf("Expected actual %f, got %f", expected, actual)
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
-	if rate != 8.0 {
-		t.Fatalf("Expected rate 8.0, got %f", rate)
-	}
+}
+
+func TestCurrencyService_ReloadRate_whenCachedValue_shouldReloadRateAndSaveIt(t *testing.T) {
+	// prepare test
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRateProvider := mocks.NewMockRateProvider(ctrl)
+	mockExchangeRepo := mocks.NewMockExchangeRateRepo(ctrl)
+
+	mockRateProvider.EXPECT().FetchRateFromAPI().Return(8.00, nil)
+
+	currencyService := NewCurrencyService(mockExchangeRepo, mockRateProvider)
+	// cache value 8.0 before the test
+	mockExchangeRepo.EXPECT().SaveRate(8.0).Times(1)
+	_, err := currencyService.GetUSDtoUAHRate()
+	assertNoError(t, err)
+
+	// perform test
+	mockRateProvider.EXPECT().FetchRateFromAPI().Return(40.9, nil)
+	rate, err := currencyService.GetUSDtoUAHRate()
+	assertRate(t, err, 8.0, rate)
+
+	mockExchangeRepo.EXPECT().SaveRate(40.9).Times(1)
+	assertNoError(t, currencyService.ReloadRate())
+	rate, err = currencyService.GetUSDtoUAHRate()
+	assertRate(t, err, 40.9, rate)
 }
